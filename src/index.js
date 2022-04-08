a5bb9f5e359bb7acdf5901e3aac27c020437d274
Macaulay Eden <macaulay.eden30@gmail.com>
2022-04-08 17:07:57 +0100

    potential bug

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ getBooksOrMoviesPromise.then(results => {
 async function getBooksAndMoviesAsync() {
     try {
         const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
-        return { books, movies }
+        return { books, movies };
     } catch(error) {
         console.log("Error fetching books and movies", error);
     }
@@ -42,20 +42,18 @@ async function getBooksOrMoviesAsync() {
         return values;
     } catch(error) {
         console.error("Error waiting for the promise race", error);
-    }
+    };
 }
 
-getBooksAndMoviesAsync()
-    .then(results => {
+getBooksAndMoviesAsync().then(results => {
         console.log("movies and books", {
             movies: results.movies,
             books: results.books
           });
-    });
-
-getBooksOrMoviesAsync()
-    .then(results => {
-        console.log("movies OR books", {
-            results
-        });
 });
+
+getBooksOrMoviesAsync().then(results => {
+    console.log("movies OR books", {
+        results
+    });
+})   
