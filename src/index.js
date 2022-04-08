dd67f594c38de565bd5cb38d0e68bddd0f310ade
Macaulay Eden <macaulay.eden30@gmail.com>
2022-04-08 15:51:40 +0100

    Part 3 attempt 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export function fetchMovies() {
+let movies = require("./data/movies.json");
+import { fetchWithTimeout } from "./services";
 
+
+export function fetchMovies() {
+    const resolveFunction  = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction)
 }
+
+const moviePromise = fetchMovies();
+
+moviePromise.then(resolve = (results) => {
+    console.log(results)
+})
